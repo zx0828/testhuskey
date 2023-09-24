97cb2ad4c44ccd9adcf9e44cca6cb2c608402b0f
zhangxiang <501696487@qq.com>
2023-09-24 17:39:59 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="box">
+    <h1>hello app</h1>
+  </div>
+</template>
+<script setup lang="ts"></script>
+<style scoped lang="scss">
+.box {
+  width: 600px;
+  height: 400px;
+  background: black;
+  h1 {
+    color: white;
+  }
+}
+</style>
